0b8c82439b037b88f56dc4a8db7032104a0f87f8
Viniciuscramos83 <114232338+Mavekas@users.noreply.github.com>
2024-05-23 20:32:37 -0300

    Update database.py

--- bot discord/database.py
+++ bot discord/database.py
@@ -39,8 +39,27 @@ async def alterar_saldo(usuario,quantidade):
     }}
 
      usuarios.update_one(filtro,relacao)
+async def remover_usuario(usuario):
+    filtro = {"discord_id": usuario.id}
+    resultado = usuarios.delete_one(filtro)
+    
+    if resultado.deleted_count == 1:
+        return True  
+    else:
+        return False  
 
+async def ranking_usuarios():
+   
+    usuarios_ranking = usuarios.find().sort("moedas", pymongo.DESCENDING)
 
+    ranking = []
+
+    for posicao, usuario in enumerate(usuarios_ranking, start=1):
+        discord_id = usuario["discord_id"]
+        moedas = usuario["moedas"]
+        ranking.append({"posicao": posicao, "discord_id": discord_id, "moedas": moedas})
+
+    return ranking
     
 
     
